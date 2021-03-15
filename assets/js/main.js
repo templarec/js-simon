d3dfe3a84174b44c6e73808191ebaba6c5626cc9
Lorenzo Bernini <templarec@gmail.com>
2021-03-15 15:56:57 +0100

    Preliminari

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+
+$(document ).ready(function() {
+//genero 5 numeri random
+//li visualizzo per 30 secondi
+//li nascondo
+//chiedo 5 numeri all'utente
+//verifico quanti ha ricordato
+
+
+
+
+});
+
+//funzione genera numeri
+function getRndInteger(min, max) {
+	return Math.floor(Math.random() * (max - min) ) + min;
+}
